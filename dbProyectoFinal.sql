9b41af26a5a59c70a221a070fcf00c8938a4d83b
KennXxXxX <Kangel-2024458@kinal.edu.gt>
2025-07-22 21:55:23 -0600

    KA-T006 Agregar entidad Pedidos y CRUD en la DB

--- dbProyectoFinal.sql
+++ dbProyectoFinal.sql
@@ -49,6 +49,18 @@ create table Productos (
     estado enum('activo', 'inactivo') default 'activo'
 );
 
+create table Pedidos (
+	idPedido int auto_increment not null,
+    idCliente int not null,
+    fechaPedido date,
+    total decimal (10,2) not null,
+    metodoPago ENUM('Efectivo','Tarjeta Credito', 'Tarjeta Debito') default 'Efectivo',
+    estado ENUM('Pendiente', 'Pagado','Enviado', 'Entregado', 'Cancelado') default 'Pendiente',
+    primary key pk_pedidos (idPedido),
+    constraint pk_pedidos_clientes foreign key (idCliente)
+		references Clientes (idCliente)
+);
+
 create table CarritoDetalles (
 	idDetalleCarrito int auto_increment not null,
     idCliente int not null,
@@ -247,6 +259,58 @@ begin
     delete from Productos where idProducto = p_idProducto;
 end $$
 
+delimiter ;
+
+delimiter $$
+-- 1. Listar Pedidos
+create procedure sp_ListarPedidos()
+begin
+    select * from Pedidos;
+end$$
+delimiter ;
+
+delimiter $$
+-- 2. Agregar Pedido
+create procedure sp_AgregarPedido(
+    in p_idCliente int,
+    in p_fechaPedido date,
+    in p_total decimal(10,2),
+    in p_metodoPago enum('Efectivo','Tarjeta Credito', 'Tarjeta Debito'),
+    in p_estado enum('Pendiente', 'Pagado','Enviado', 'Entregado', 'Cancelado')
+)
+begin
+    insert into Pedidos(idCliente, fechaPedido, total, metodoPago, estado)
+    values (p_idCliente, p_fechaPedido, p_total, p_metodoPago, p_estado);
+end$$
+delimiter ;
+
+delimiter $$
+-- 3. Actualizar Pedido
+create procedure sp_ActualizarPedido(
+    in p_idPedido int,
+    in p_idCliente int,
+    in p_fechaPedido date,
+    in p_total decimal(10,2),
+    in p_metodoPago enum('Efectivo','Tarjeta Credito', 'Tarjeta Debito'),
+    in p_estado enum('Pendiente', 'Pagado','Enviado', 'Entregado', 'Cancelado')
+)
+begin
+    update Pedidos
+    set idCliente = p_idCliente,
+        fechaPedido = p_fechaPedido,
+        total = p_total,
+        metodoPago = p_metodoPago,
+        estado = p_estado
+    where idPedido = p_idPedido;
+end$$
+delimiter ;
+
+delimiter $$
+-- 4. Eliminar Pedido
+create procedure sp_EliminarPedido(in p_idPedido int)
+begin
+    delete from Pedidos where idPedido = p_idPedido;
+end$$
 delimiter ;
 
 delimiter $$
