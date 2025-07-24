618a317a1538fc27ff6bdb9a1442dd651bb08714
Saul Sical <ssical-2024422@kinal.edu.gt>
2025-07-23 20:09:17 -0600

    T000. componer errores de base da datos

--- src/main/java/controller/dbProyectoFinal.sql
+++ src/main/java/controller/dbProyectoFinal.sql
@@ -64,6 +64,106 @@ create table CarritoDetalles (
 		references Productos (idProducto)
 );
 
+create table Pedidos (
+	idPedido int auto_increment not null,
+    idCliente int not null,
+    fechaPedido date,
+    total decimal (10,2) not null,
+    metodoPago ENUM('Efectivo','Tarjeta Credito', 'Tarjeta Debito') default 'Efectivo',
+    estado ENUM('Pendiente', 'Pagado','Enviado', 'Entregado', 'Cancelado') default 'Pendiente',
+    primary key pk_pedidos (idPedido),
+    constraint pk_pedidos_clientes foreign key (idCliente)
+		references Clientes (idCliente)
+);
+
+create table Detallepedidos(
+	idDetallePedido int auto_increment not null, 
+    idPedido int not null, 
+    idProducto int not null, 
+    cantidad int not null, 
+    precioUnitario double, 
+    subTotal double, 
+    constraint pk_detallePedido primary key (idDetallePedido), 
+    
+    constraint fk_detallePedido_Pedidos foreign key (idPedido) 
+		references Pedidos(idPedido),
+	constraint fk_detallePedido_Producto foreign key (idProducto) 
+		references Productos(idProducto)
+); 
+
+create table Envios(
+	idEnvio int auto_increment not null, 
+    idPedido int not null, 
+    direccionEnvio varchar(250), 
+    empresaEnvio varchar(250), 
+    codigoSeguimiento int, 
+    fechaEnvio datetime, 
+    fechaEntrega datetime, 
+    estado enum('Pendiente','En tránsito','Entregado'), 
+    constraint pk_envios primary key(idEnvio),
+    
+    constraint fk_envios_Pedidos foreign key (idPedido) 
+		references Pedidos (idPedido)
+);
+
+create table CarritoDetalles (
+	idDetalleCarrito int auto_increment not null,
+    idCliente int not null,
+@@ -249,6 +261,58 @@ end $$
+
+delimiter ;
+
+delimiter $$
+-- 1. Listar Pedidos
+create procedure sp_ListarPedidos()
+begin
+    select * from Pedidos;
+end$$
+delimiter ;
+
+delimiter $$
+-- 2. Agregar Pedido
+create procedure sp_AgregarPedido(
+    in p_idCliente int,
+    in p_fechaPedido date,
+    in p_total decimal(10,2),
+    in p_metodoPago enum('Efectivo','Tarjeta Credito', 'Tarjeta Debito'),
+    in p_estado enum('Pendiente', 'Pagado','Enviado', 'Entregado', 'Cancelado')
+)
+begin
+    insert into Pedidos(idCliente, fechaPedido, total, metodoPago, estado)
+    values (p_idCliente, p_fechaPedido, p_total, p_metodoPago, p_estado);
+end$$
+delimiter ;
+
+delimiter $$
+-- 3. Actualizar Pedido
+create procedure sp_ActualizarPedido(
+    in p_idPedido int,
+    in p_idCliente int,
+    in p_fechaPedido date,
+    in p_total decimal(10,2),
+    in p_metodoPago enum('Efectivo','Tarjeta Credito', 'Tarjeta Debito'),
+    in p_estado enum('Pendiente', 'Pagado','Enviado', 'Entregado', 'Cancelado')
+)
+begin
+    update Pedidos
+    set idCliente = p_idCliente,
+        fechaPedido = p_fechaPedido,
+        total = p_total,
+        metodoPago = p_metodoPago,
+        estado = p_estado
+    where idPedido = p_idPedido;
+end$$
+delimiter ;
+
+delimiter $$
+-- 4. Eliminar Pedido
+create procedure sp_EliminarPedido(in p_idPedido int)
+begin
+    delete from Pedidos where idPedido = p_idPedido;
+end$$
+delimiter ;
 
 delimiter $$
 -- 1. Listar Usuarios
